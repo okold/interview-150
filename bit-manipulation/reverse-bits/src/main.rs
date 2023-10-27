05006600958bb236b908f3d0a332029f3e418fe5
olga <o.koldachenko@gmail.com>
2023-10-26 21:06:40 -0600

    less cheesy bit reverse

--- bit-manipulation/reverse-bits/src/main.rs
+++ bit-manipulation/reverse-bits/src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
-    // also probably too cheesy
+    // ok less cheesy than using built in reverse_bits
     pub fn reverse_bits(x: u32) -> u32 {
-        return x.reverse_bits();
+        let mut worker = x;
+        let mut rev = 0u32;
+
+        for _ in 0..32 {
+            // move rev to left and insert 1 if worker's least significant digit is a 1
+            rev =  rev << 1 | worker & 1;
+            // move worker right, so lsd is next lsd
+            worker >>= 1;
+        }
+
+        rev
     }
 
     assert_eq!(reverse_bits(0b00000010100101000001111010011100), 964176192);
